6bc3b1c8c982b48a5cc23a65874313ebac7f048e
Sahil Agarwal <agarwalsahil0210@gmail.com>
2021-09-02 15:38:59 +0530

    added loading page in community section

--- my-app/src/core/Members.js
+++ my-app/src/core/Members.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from "react";
 import Base from "./Base";
 import UserCard from "./userCard";
 import "../Styles/members.css";
+import Spinner from "./Spinner";
 let cord;
 let teach;
 let memb;
+
 const Members = () => {
+  const [loadSpinner, setLoadSpinner] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadSpinner(false);
+    }, 1000);
+  }, []);
   const [members, setMembers] = useState([]);
   useEffect(() => {
     fetchItems();
@@ -31,78 +39,82 @@ const Members = () => {
     });
     console.log(cord);
   };
-  return (
-    <div>
-      {check()}
-      <Base>
-        <div>
-          <section>
-            <div className="container-x">
-              <div className="container1-x">
-                <h1>Meet our community</h1>
-                <h2>Teacher coordinators</h2>
+  const getMemberData = () => {
+    return (
+      <div>
+        {check()}
+        <Base>
+          <div>
+            <section>
+              <div className="container-x">
+                <div className="container1-x">
+                  <h1>Meet our community</h1>
+                  <h2>Teacher coordinators</h2>
+                </div>
               </div>
-            </div>
-          </section>
-          <section>
-            <div className="container-x">
-              <div className="split1-x">
-                {/* <div>
+            </section>
+            <section>
+              <div className="container-x">
+                <div className="split1-x">
+                  {/* <div>
                   <UserCard />
                 </div>
                 <div>
                   <UserCard />
                 </div> */}
-                {teach.map((member) => (
-                  <UserCard
-                    img={member.githubDP}
-                    name={member.name}
-                    skill={member.skill}
-                    twitterLink={member.twitter}
-                    linkedinLink={member.linkedin}
-                    githubLink={member.github}
-                  />
-                ))}
+                  {teach.map((member) => (
+                    <UserCard
+                      img={member.githubDP}
+                      name={member.name}
+                      skill={member.skill}
+                      twitterLink={member.twitter}
+                      linkedinLink={member.linkedin}
+                      githubLink={member.github}
+                    />
+                  ))}
+                </div>
               </div>
-            </div>
-          </section>
-          <section>
-            <div className="container-x">
-              <h2>Current Coordinators</h2>
-              <div className="splitx">
-                {cord.map((member) => (
-                  <UserCard
-                    img={member.githubDP}
-                    name={member.name}
-                    skill={member.skill}
-                    twitterLink={member.twitter}
-                    linkedinLink={member.linkedin}
-                    githubLink={member.github}
-                  />
-                ))}
+            </section>
+            <section>
+              <div className="container-x">
+                <h2>Current Coordinators</h2>
+                <div className="splitx">
+                  {cord.map((member) => (
+                    <UserCard
+                      img={member.githubDP}
+                      name={member.name}
+                      skill={member.skill}
+                      twitterLink={member.twitter}
+                      linkedinLink={member.linkedin}
+                      githubLink={member.github}
+                    />
+                  ))}
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section className="member-section">
-            <h2 className="class1">Members</h2>
-            <div className="split2-x">
-              {memb.map((member) => (
-                <UserCard
-                  img={member.githubDP}
-                  name={member.name}
-                  skill={member.skill}
-                  twitterLink={member.twitter}
-                  linkedinLink={member.linkedin}
-                  githubLink={member.github}
-                />
-              ))}
-            </div>
-          </section>
-        </div>
-      </Base>
-    </div>
-  );
+            <section className="member-section">
+              <h2 className="class1">Members</h2>
+              <div className="split2-x">
+                {memb.map((member) => (
+                  <UserCard
+                    img={member.githubDP}
+                    name={member.name}
+                    skill={member.skill}
+                    twitterLink={member.twitter}
+                    linkedinLink={member.linkedin}
+                    githubLink={member.github}
+                  />
+                ))}
+              </div>
+            </section>
+          </div>
+        </Base>
+      </div>
+    );
+  };
+
+  return <>{loadSpinner ? <Spinner /> : getMemberData()}</>;
 };
 
 export default Members;
