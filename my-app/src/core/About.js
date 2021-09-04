82f8311368a7c8c938b2465eaa8962bee39c1a25
Anshuman Mohapatra <amohapatra2001@gmail.com>
2021-09-04 15:37:18 +0530

    about us

--- my-app/src/core/About.js
+++ my-app/src/core/About.js
@@ -1,38 +1,39 @@
-import React, { useState } from "react";
+import React
+// , { useState }
+  from "react";
 import Base from "./Base"
 import Styles from "../Styles/About.module.css";
-import banner from "../Assets/Images/banner.svg";
-import emailSvg from "../Assets/Icons/email.svg"
-import CustomInput from "../Components/Input";
+import banner from "../Assets/Images/about.jpg";
+// import emailSvg from "../Assets/Icons/email.svg"
+// import CustomInput from "../Components/Input";
 import CustonButton from "../Components/Buttons"
 const About = () => {
-    const [email, setEmail] = useState("");
+    // const [email, setEmail] = useState("");
     const handleClick = () => {
-        
+        window.open("https://docs.google.com/forms/d/e/1FAIpQLSdOp_wsPsjwFvxhSRECAxBsUGeL2s4cjJ1SghNLgNPg7f8bHQ/viewform")
     }
     return (
       <Base>
         <div className={Styles.about_outer}>
           <div className={Styles.about_banner}>
-            <div>
+      
               <img src={banner} alt='' />
-            </div>
+         
           </div>
           <div className={Styles.about_content}>
             <div className={Styles.content_left}>
               <div className={Styles.content_heading}>Our Story!</div>
               <div className={Styles.content_para}>
-                Mauris porttitor fermentum massa, non ullamcorper turpis ornare
-                nec. Suspendisse arcu odio, hendrerit non tempor vel, gravida ac
-                libero. Aliquam accumsan bibendum ante. Cras quis urna at augue
-                convallis interdum.
+                We here at codex are identified as coders only because we know what it takes to start from scratch and keep on scratching till we achieve our goals.<br/>
+                We have seen our aluminis sharpen their skills, work in teams, brainstorm ideas and get placed in fortune companies.
+                Our goal is not to teach anyone rather, learn together.
               </div>
             </div>
             <div className={Styles.content_right}>
               <div className={Styles.newsletter_heading}>
                 Join our monthly Newsletter
               </div>
-              <div className={Styles.input_email}>
+              {/* <div className={Styles.input_email}>
                 <CustomInput
                   name='email'
                   value={email}
@@ -42,7 +43,7 @@ const About = () => {
                   handleChange={(e) => setEmail(e.target.value)}
                   svg={emailSvg}
                 />
-              </div>
+              </div> */}
               <div className={Styles.button_margin}>
                 <CustonButton
                   text='Subscribe'
