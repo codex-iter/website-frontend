5f3dad66c1ffff66a684d6f94a8bb42588585ff9
majjikishore007 <majjikishore031@gmail.com>
2021-09-05 11:20:03 +0530

    updated

--- my-app/src/core/Event.js
+++ my-app/src/core/Event.js
@@ -1,101 +1,121 @@
-
+import React, { useState, useEffect } from "react";
 import bg from "../Assets/Images/bg.jpg";
 import Styles from "../Styles/Event.css";
 import Base from "./Base";
 import EventSilder from "./EventSilder";
 /*events page*/
 function Event() {
-    return (
-        <Base>
-            <section className="bgevent">
-                <div class="heading1">
-                    <h1>Events Organized By Our Community</h1>
-                </div>
-                <div className="container-y">
-                    <div className="split-y">
-                        <div className="card_content">
-                            <h1>Event 1</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                <p><b class="organizers">Organizers</b><br>
-                                </br>
-                                    Benito Noboa,Benito Noboa,Benito Noboa
-                                </p>
-                            </p>
-                        </div>
-                        <div >
-                            <EventSilder key={1}>
-                            </EventSilder>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="bgeevent1">
-                <div class="container-y">
-                    <div class="split-y">
-                        <div>
-                            <EventSilder key={2}>
-
-                            </EventSilder>
-                        </div>
-                        <div className="card_content">
-                            <h1>Event 2</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                <p><b class="organizers">Organizers</b><br>
-                                </br>
-                                    Benito Noboa,Benito Noboa,Benito Noboa
-                                </p>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section className="bgevent">
-                <div className="container-y">
-                    <div className="split-y">
-                        <div className="card_content">
-                            <h1>Event 3</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                <p><b class="organizers">Organizers</b><br>
-                                </br>
-                                    Benito Noboa,Benito Noboa,Benito Noboa
-                                </p>
-                            </p>
-                        </div>
-                        <div>
-                            <EventSilder key={3}></EventSilder>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="bgeevent1">
-                <div class="container-y">
-                    <div class="split-y">
-                        <div>
-                            <EventSilder key={4}></EventSilder>
-                        </div>
-                        <div className="card_content">
-                            <h1>Event 4</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                <p><b class="organizers">Organizers</b><br>
-                                </br>
-                                    Benito Noboa,Benito Noboa,Benito Noboa
-                                </p>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-
-        </Base>
+  const [photos, setPhotos] = useState({});
+  const getEventsData = async () => {
+    const response = await fetch(
+      "https://codexweb-backend.herokuapp.com/api/getEvents"
     );
+    const data = await response.json();
+    console.log("data", data[0].images);
+    setPhotos(data);
+    console.log("Photos", photos[0].description);
+  };
+  useEffect(() => {
+    getEventsData();
+  }, []);
+  return (
+    <Base>
+      <section className='bgevent'>
+        <div class='heading1'>
+          <h1>Events Organized By Our Community</h1>
+        </div>
+        <div className='container-y'>
+          <div className='split-y'>
+            <div className='card_content'>
+              <h1>Event 1</h1>
+              <p>
+                {photos[0].description}
+                <p>
+                  <b class='organizers'>Organizers</b>
+                  <br></br>
+                  {photos[0].organizers.map((o, i) => (
+                    <span>{o} ,</span>
+                  ))}
+                </p>
+              </p>
+            </div>
+            <div>
+              <EventSilder photos={photos}></EventSilder>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class='bgeevent1'>
+        <div class='container-y'>
+          <div class='split-y'>
+            <div>
+              <EventSilder photos={photos}></EventSilder>
+            </div>
+            <div className='card_content'>
+              <h1>Event 2</h1>
+              <p>
+                {photos[0].description}
+                <p>
+                  <b class='organizers'>Organizers</b>
+                  <br></br>
+                  {photos[0].organizers.map((o, i) => (
+                    <span>{o} ,</span>
+                  ))}
+                </p>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='bgevent'>
+        <div className='container-y'>
+          <div className='split-y'>
+            <div className='card_content'>
+              <h1>Event 3</h1>
+              <p>
+                {photos[0].description}
+                <p>
+                  <b class='organizers'>Organizers</b>
+                  <br></br>
+                  {photos[0].organizers.map((o, i) => (
+                    <span>{o} ,</span>
+                  ))}
+                </p>
+              </p>
+            </div>
+            <div>
+              <EventSilder photos={photos}></EventSilder>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class='bgeevent1'>
+        <div class='container-y'>
+          <div class='split-y'>
+            <div>
+              <EventSilder photos={photos}></EventSilder>
+            </div>
+            <div className='card_content'>
+              <h1>Event 4</h1>
+              <p>
+                {photos[0].description}
+                <p>
+                  <b class='organizers'>Organizers</b>
+                  <br></br>
+                  {photos[0].organizers.map((o, i) => (
+                    <span>{o} ,</span>
+                  ))}
+                </p>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Base>
+  );
 }
 
 export default Event;
--- my-app/src/core/EventSilder.js
+++ my-app/src/core/EventSilder.js
@@ -1,39 +1,95 @@
-import React from 'react'
-import bg from "../Assets/Images/bg.jpg";
+import React from "react";
+import { Carousel } from "react-carousel-minimal";
+import bg from "../Assets/Images/user.png";
+const data = [
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+    caption: `<div>
+                  San Francisco
+                  <br/>
+                  Next line
+                </div>`,
+  },
+  {
+    image:
+      "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+    caption: "Scotland",
+  },
+  {
+    image:
+      "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+    caption: "Darjeeling",
+  },
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
+    caption: "San Francisco",
+  },
+  {
+    image:
+      "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
+    caption: "Scotland",
+  },
+  {
+    image:
+      "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
+    caption: "Darjeeling",
+  },
+  {
+    image:
+      "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
+    caption: "San Francisco",
+  },
+  {
+    image:
+      "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
+    caption: "Scotland",
+  },
+  {
+    image:
+      "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+    caption: "Darjeeling",
+  },
+];
+const captionStyle = {
+  fontSize: "2em",
+  fontWeight: "bold",
+};
+const slideNumberStyle = {
+  fontSize: "20px",
+  fontWeight: "bold",
+};
+
 const EventSilder = (props) => {
-    const { key } = props;
-    return (
-        <div>
-
-            <div id={key} className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img className="d-block w-100" src={bg} id="image1" alt="First slide" />
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src={bg} id="image2" alt="Second slide" />
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src={bg} id="image3" alt="Third slide" />
-                    </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="sr-only">Next</span>
-                </a>
-
-            </div>
-        </div>
-    )
-}
-
-export default EventSilder
+  const { photos } = props;
+  console.log("slider");
+  return (
+    <Carousel
+      data={data}
+      time={3000}
+      width='550px'
+      height='350px'
+      captionStyle={captionStyle}
+      radius='10px'
+      slideNumber={true}
+      slideNumberStyle={slideNumberStyle}
+      captionPosition='bottom'
+      automatic={true}
+      dots={true}
+      pauseIconColor='white'
+      pauseIconSize='40px'
+      slideBackgroundColor='darkgrey'
+      slideImageFit='cover'
+      // thumbnails={true}
+      thumbnailWidth='100px'
+      style={{
+        textAlign: "center",
+        maxWidth: "850px",
+        maxHeight: "500px",
+        margin: "40px auto",
+      }}
+    />
+  );
+};
+export default EventSilder;
