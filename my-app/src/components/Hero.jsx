4823d1fe9afe0a2fb760cf4bd39791a979be0047
Kodiererin <iamujjwal45@gmail.com>
2023-10-20 23:28:48 +0530

    Added Animation at Hero Section

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import { motion } from "framer-motion";
 import ContactInfo from "./ContactInfo";
 import siteConfig from "../site.config";
-
 import banner from "../assets/images/banner.png";
 import { Icon } from "@iconify/react";
 import githubIcon from "@iconify/icons-cib/github";
 
 export default function Hero() {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const leftVariants = {
+    hidden: { x: -100 },
+    visible: { x: 0, transition: { type: "spring", damping: 10 } },
+  };
+
+  const rightVariants = {
+    hidden: { x: 100 },
+    visible: { x: 0, transition: { type: "spring", damping: 10 } },
+  };
+
   return (
     <div className="pb-8 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
       <main className="pt-4 mx-auto max-w-7xl px-4 sm:px-6 md:pt-8 lg:pt-10 lg:px-8 xl:pt-14">
-        <div className="flex flex-col-reverse lg:flex-row justify-between gap-8">
-          <div className="text-center lg:text-left lg:w-1/2">
+        <motion.div
+          className="flex flex-col-reverse lg:flex-row justify-between gap-8"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.div
+            className="text-center lg:text-left lg:w-1/2"
+            variants={leftVariants}
+          >
             <p className="mb-3 text-base text-slate-400 sm:mb-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
               A Community of Coders from ITER
             </p>
@@ -35,11 +58,14 @@ export default function Hero() {
                 </a>
               </div>
             </div>
-          </div>
-          <div className="h-full lg:mt-0 flex items-center justify-center lg:w-1/2">
+          </motion.div>
+          <motion.div
+            className="h-full lg:mt-0 flex items-center justify-center lg:w-1/2"
+            variants={rightVariants}
+          >
             <img className="max-w-2xl w-full h-full" src={banner} alt="" />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </main>
     </div>
   );
