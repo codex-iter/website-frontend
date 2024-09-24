af1c810b534aec07dee4c372a7a417d49a1e49e6
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-25 01:47:49 +0530

    Updated Config

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,13 @@
+import {resolve} from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const outDir = resolve(__dirname, "../build")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: outDir
+  }
 })
