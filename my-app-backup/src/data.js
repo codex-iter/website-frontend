fe9be440578b5f6ec420277cbfbdaa3f7c98553d
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-25 01:37:54 +0530

    Remove Redundant Files

--- my-app-backup/src/data.js
+++
@@ -1,8 +0,0 @@
-const navigation = [
-  { name: "Home", to: "/" },
-  { name: "Community", to: "/community" },
-  { name: "Events", to: "/events" },
-  { name: "About us", to: "about-us" },
-  { name: "Contact us", to: "/contact-us" },
-];
-export { navigation };
--- my-app-backup/src/lib/constants.js
+++
@@ -1,3 +0,0 @@
-const API_URL = "https://website-backend-blue.vercel.app";
-
-export { API_URL };
